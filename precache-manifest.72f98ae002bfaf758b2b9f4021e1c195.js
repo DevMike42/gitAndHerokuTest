b6a7991a05ddef15ad5cd758e8a4822d250ecd64
Mike Rivera <michaelstephenrivera@gmail.com>
2019-06-18 12:45:57 -0700

    Updates

---
+++ precache-manifest.72f98ae002bfaf758b2b9f4021e1c195.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a509ec84386c0f16340a6de68fdde2c",
+    "url": "/gitAndHerokuTest/index.html"
+  },
+  {
+    "revision": "e5bc702a3bef3473c4ca",
+    "url": "/gitAndHerokuTest/static/css/main.f9c67ae1.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/gitAndHerokuTest/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "e5bc702a3bef3473c4ca",
+    "url": "/gitAndHerokuTest/static/js/main.bfd9a2a7.chunk.js"
+  },
+  {
+    "revision": "2209aa734bd647ff5b8c",
+    "url": "/gitAndHerokuTest/static/js/runtime~main.b8c5f377.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gitAndHerokuTest/static/media/logo.5d5d9eef.svg"
+  }
+]);
